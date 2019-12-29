66d1a6858560a873ee807363a1b83264e31f8852
slipknotts <55863113+slipknotts@users.noreply.github.com>
2019-12-28 18:54:32 -0800

    Update apache.sh
    added enable to apache service so its ready to start when you call it

--- apache.sh
+++ apache.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 yum -y install httpd mod_ssl
+systemctl enable httpd
 systemctl start httpd
+
 sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
 
 echo "<html><body> /n<h1>NTI-300</h1> /n<h2>Automation and Scripting with Linux</h2> /n</body></html>" > /var/www/html/index.html
