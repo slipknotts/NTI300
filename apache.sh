71d3b9863dedf63e10f918470c70d5c3b8cc1aea
slipknotts <55863113+slipknotts@users.noreply.github.com>
2019-12-28 17:17:57 -0800

    Create apache.sh
    Automating the additional installation of the Apache package (httpd)

---
+++ apache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+yum -y install httpd mod_ssl
+systemctl start httpd
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
+
+echo "<html><body> /n<h1>NTI-300</h1> /n<h2>Automation and Scripting with Linux</h2> /n</body></html>" > /var/www/html/index.html
