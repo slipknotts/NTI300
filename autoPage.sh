4655a00bb489a4b857a74c7b498d1bcc0281bc4a
slipknotts <55863113+slipknotts@users.noreply.github.com>
2019-12-28 17:07:36 -0800

    Create autoPage.sh
    uses %c which gives full date and time NOT to be confused with %C which will only give the year with 2 digits instead of 4

---
+++ autoPage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo -e "<html><body> /n<h1>NTI-300</h1> /n<h2>Automating Linux is Super-Duper FUN!!!</h2> /n<p>Current System Timestamp  ...  $(date +%c)</p> /n</body></html> > /var/www/html/index.html
